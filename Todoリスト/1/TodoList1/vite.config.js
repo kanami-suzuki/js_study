0aa3e5d1e2bb63a4c6e69408efcd2cdb34eb558e
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-03-25 22:06:49 +0900

    環境構築

--- Todoリスト/1/TodoList1/vite.config.js
+++ Todoリスト/1/TodoList1/vite.config.js
@@ -3,5 +3,25 @@ import {
 } from "vite";
 
 export default defineConfig({
-  //ここに設定を追加
+  root: "src",
+  build: {
+    outDir: "../dist", //プロジェクトルートからの相対パス(index.pugからの相対パス)
+    emptyOutDir: true,
+    rollupOptions: {
+      input: "src/index.html",
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(assetInfo.name)) {
+            return 'assets/images/[name].[ext]';
+          }
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
+  }
 })
