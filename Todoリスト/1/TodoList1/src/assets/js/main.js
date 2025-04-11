3cd4c5eb7c081f501eb7b154f5973d3a1fe4b6ac
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-04-11 21:22:05 +0900

    js

--- Todoリスト/1/TodoList1/src/assets/js/main.js
+++ Todoリスト/1/TodoList1/src/assets/js/main.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', function () {
   const top = this.document.querySelector('#top');
   const bottom = this.document.querySelector('#bottom');
+  const todoList = document.querySelector('.todoList');
+  const todoText = document.querySelector('#todoText');
+  const todoButton = document.querySelector('#todoButton');
+  const todoBottom = document.querySelector('#todoBottom');
 })
