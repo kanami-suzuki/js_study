d9b1553a0bff80e09ffcb80bbad544b7ea881a18
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-04-14 21:01:05 +0900

    JS

--- Todoリスト/1/TodoList1/src/assets/js/main.js
+++ Todoリスト/1/TodoList1/src/assets/js/main.js
@@ -1,14 +1,19 @@
 console.log('Hello!');
-const top = document.querySelector('#top');
-const bottom = document.querySelector('#bottom');
+let top = document.querySelector('#top');
+let bottom = document.querySelector('#bottom');
 const todoList = document.querySelector('.todoList');
 const todoText = document.querySelector('#todoText');
 const todoButton = document.querySelector('#todoButton');
 const todoBottom = document.querySelector('#todoBottom');
 
+top = 0;
+bottom = 0;
 todoButton.addEventListener('click', () => {
   let newLi = document.createElement('li');
   newLi.textContent = todoText.value;
 
-  todoList.appendChild(newLi)
+  todoList.appendChild(newLi);
+  todoText.value = "";
+  top = top++;
+  bottom = bottom++;
 })
