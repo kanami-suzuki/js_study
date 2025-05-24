3673aadc4b8f9b1d9e259b0d7919d5f84cda35bd
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-05-24 22:42:27 +0900

    js

---
+++ Todoリスト/1/TodoList2/vite-project/src/assets/main.js
@@ -0,0 +1,7 @@
+const todoList = document.querySelector('.todoList');
+const todoText = document.querySelector('#todoText');
+const todoButton = document.querySelector('#todoButton');
+const todoBottom = document.querySelector('#todoBottom');
+
+const topCount = document.querySelector('#top');
+const bottomCount = document.querySelector('#bottom');
